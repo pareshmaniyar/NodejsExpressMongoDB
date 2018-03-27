23b978297631914edaed3c9a62896cfbd3e7388c
Mann <pareshmaniyar1@gmail.com>
2018-03-27 11:18:14 +0530

    Express Example One

--- node-http/index.js
+++ node-http/index.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 const path = require('path');
 const hostname = 'localhost';
 const port = 3000;
-const server = http.createServer((req,res) =>{//setup the server
+const server = http.createServer((req,res) =>{														//setup the server
 	console.log("Request for" + req.url + " and method " + req.method);
-	if(req.method == 'GET'){//Step 1: check the method
+	if(req.method == 'GET'){																//Step 1: check the method
 		var fileURL;
-		if(req.url == '/') fileURL = '/index.html'; // Step 2: check the URL, set default
+		if(req.url == '/') fileURL = '/index.html'; 							 // Step 2: check the URL, set default
 		else fileURL = req.url;
 
-		var filePath = path.resolve('./public'+fileURL);//S3: translate into full fledged path
+		var filePath = path.resolve('./public'+fileURL);						//S3: translate into full fledged path
 		const fileExt = path.extname(filePath); 
-		if(fileExt == '.html'){ //S4: check file extension
-			fs.exists(filePath, (exists) => {//first callback function, with exists method
-				if(!exists){//if file doesn't exists
+		if(fileExt == '.html'){ 															//S4: check file extension
+			fs.exists(filePath, (exists) => {							 //first callback function, with exists method
+				if(!exists){																  //if file doesn't exists
 					res.statusCode = 404;//status code
           			res.setHeader('Content-Type', 'text/html');//header-content-type
           			res.end('<html><body><h1>Error 404: ' + fileURL + ' not found</h1></body></html>');
@@ -22,7 +22,7 @@ const server = http.createServer((req,res) =>{//setup the server
         		}
         		res.statusCode = 200;
     			res.setHeader('Content-Type', 'text/html');
-		        fs.createReadStream(filePath).pipe(res);//reading file and sending in response body message
+		        fs.createReadStream(filePath).pipe(res);			//reading file and sending in response body message
 			});
 		}else{
 			res.statusCode = 404;
