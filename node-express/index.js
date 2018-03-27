078b7527cca5d6c04dbbcfb75de9d6ce2afec80b
Mann <pareshmaniyar1@gmail.com>
2018-03-27 11:50:18 +0530

    Express serve static files

--- node-express/index.js
+++ node-express/index.js
@@ -1,10 +1,13 @@
-const express = require('express');//it is third party module
+const express = require('express');						//it is third party module
 const http = require('http');
+const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));			//static html files will be served
 app.use((req,res,next) => {
-	console.log(req.headers);
+	//console.log(req.headers);
 	res.statuscode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	res.end('<html><body><h1>This is an Express Server</h1></body></html>');
