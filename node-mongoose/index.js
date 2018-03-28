79d959d5e52eac16e97167de56668d6c739a4649
Mann <pareshmaniyar1@gmail.com>
2018-03-28 16:22:43 +0530

    Node Mongoose Method 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -3,28 +3,32 @@ mongoose.Promise = require('bluebird');
 const Dishes = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
-connect.then((db) => {
+connect.then( (db) => {
 	console.log("Connected successfully to the server");
-	var newDish = Dishes({ name: 'Paresh123456',	description: 'Maniyar123456'	});//create model
-	newDish.save()						//save model
-	.then((dish) => {
-		console.log('Dish saved: ',dish);
-		var secDish = Dishes({name: 'Paresh7', description: 'Maniyar7'});
-		secDish.save()
-		.then((dish) => {
-			console.log('Dish saved: ',dish);
-			return Dishes.find({}).exec();	//finds everthing, exec() ensures execution
+	Dishes.create({ name: 'Paresh1234567', description: 'Maniyar1234567'	//create model type 2
+	})   
+	.then( (dish) => {
+		console.log('Dish 1 saved: ',dish);
+		Dishes.create({
+			name: 'Paresh7', description: 'Maniyar7'
 		})
-		.then((dishes) => {
-		console.log('found dishes',dishes);
-		return mongoose.connection.db.collection('dishes').drop();
+		.then( (dish2) => {
+		console.log('Dish 2 saved: ',dish2);
+		return Dishes.find({}).exec();	//finds everthing, exec() ensures execution
 		})
-		.then(() => {
+		.then( (dishes) => {
+			console.log('found dishes',dishes);
+			return mongoose.connection.db.collection('dishes').drop();
+		})
+		.then( () => {
 			console.log('dropped dishes');
 			return mongoose.connection.close();
 		})
 		.catch((err) => {
 			console.log(err);
 		});
+	})
+	.catch((err) => {
+			console.log(err);
 	});
 });
