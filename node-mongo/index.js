7218ad0a1dd15d9c170f20a4b19c34fc38a1da94
Mann <pareshmaniyar1@gmail.com>
2018-03-28 11:24:13 +0530

    Node Call-Back Hell and Promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -2,30 +2,34 @@ const MongoClient = require('mongodb').MongoClient;//connect to the mongoDB serv
 const assert = require('assert');            //to see the true/false value for err
 const url = 'mongodb://localhost:27017';     //server hosted
 const dboper = require('./operations');
-MongoClient.connect(url, (err, client) => {  //db gives access to database
+MongoClient.connect(url).then((client) => {  //db gives access to database
     var db = client.db('conFusion');                    //1: client object containing the database object
-    assert.equal(err, null); // if connected properly
     console.log('Connected Successfully to MongoDb server');
     //Step 1: Insert
-    dboper.insertDocument(db, { "name": "Paresh", "description": "Maniyar"}, "dishes", (result) => {
+    dboper.insertDocument(db, { "name": "Paresh", "description": "Maniyar"}, "dishes")
+    .then( (result) => {
         console.log("Inserted Document:\n", result.ops);
         //Step 2: Find
-        dboper.findDocuments(db, "dishes", (docs) => {
-            console.log("Found these Documents:\n", docs);
-            //Step 3: Update
-            dboper.updateDocuments(db, {name: "Paresh" }, {description: "Maniyar Updated"}, "dishes", (result) => {
-                console.log("Update Completed: \n", result.result);//property which shows what has been updated
-                //Step 4: Find Again
-                dboper.findDocuments(db, "dishes", (docs) => {
-                    console.log("Found Updated Documents:\n", docs);
-                    //Step 5: Drop collection
-                    db.dropCollection("dishes", (result) => {
-                        console.log("Dropped Collection: ", result);
-                        //Step 6: Close the connection with the MongoDB server
-                        client.close();//client method in Mongo 3 version
-                    });
-                });
-            });
-        });
+        return dboper.findDocuments(db, "dishes");
+    })
+    .then((docs) => {
+        console.log("Found these Documents:\n", docs);
+        //Step 3: Update
+        return dboper.updateDocuments(db, {name: "Paresh" }, {description: "Maniyar Updated"}, "dishes");
+    })
+    .then( (result) => {
+        console.log("Update Completed: \n", result.result);//property which shows what has been updated
+        //Step 4: Find Again
+        return dboper.findDocuments(db, "dishes");
+    })
+    .then( (docs) => {
+        console.log("Found Updated Documents:\n", docs);
+        //Step 5: Drop collection
+        return db.dropCollection("dishes");
+    })
+    .then( (result) => {
+        console.log("Dropped Collection: ", result);
+        //Step 6: Close the connection with the MongoDB server
+        client.close();//client method in Mongo 3 version
     });
 });
--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -5,35 +5,17 @@ exports.insertDocument = (db, document, collection, callback) => {
 	//4(mongodb database connection, document to be inserted,
 	//collection in which document has to be inserted, status once the operation is completed: a callback funtion  )
 	const coll = db.collection(collection);//find collection in db connection and assign a value
-	coll.insert(document, (err, result) => { //insert the document in using insert method(Mongo driver i guess)
-		assert.equal(err, null);
-		console.log("Inserted " + result.result.n +" documents into the collection " + collection);
-		//specifies no of documents inserted, and prints the collection
-		callback(result);//it sends result of whole collection to the index.js file after completion
-		//above object has various properties like ops which states all the inserted operations
-	});
-
+	return coll.insert(document);
 };
 exports.findDocuments = (db, collection, callback) => {//3
 	const coll = db.collection(collection);
-	coll.find({}).toArray((err, docs) => {//find the whole document
-		assert.equal(err, null);// nothing is being printed
-		callback(docs);//sends whole collection
-	});
+	return coll.find({}).toArray();
 };
 exports.removeDocuments = (db, document, collection, callback) => {//4
 	const coll = db.collection(collection);
-	coll.deleteOne(document, (err, result) => {
-		assert.equal(err, null);
-		console.log("Removed the document ", document);
-        callback(result);//sends whole collection which might be null if empty
-	});
+	return coll.deleteOne(document);
 };
 exports.updateDocuments = (db, document, update, collection, callback) => {//5
 	const coll = db.collection(collection);
-	coll.updateOne(document, { $set: update}, null, (err, result) => {
-		assert.equal(err, null);
-        console.log("Updated the document with ", update);
-        callback(result);
-	});
+	return coll.updateOne(document, { $set: update}, null);
 };
