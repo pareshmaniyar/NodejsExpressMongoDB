10634d5011373525e8d650155c94c3a55413a9ad
Mann <pareshmaniyar1@gmail.com>
2018-03-28 18:42:21 +0530

    Node Mongoose Adding Sub-Documents 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -6,18 +6,36 @@ const connect = mongoose.connect(url);
 connect.then( (db) => {
 	console.log("Connected successfully to the server");
 	Dishes.create({ name: 'Paresh1234567', description: 'Maniyar1234567'	//create model type 2
-	})   
+	})
 	.then( (dish) => {
-		console.log('Dish 1 saved: ',dish);
+		console.log('Dish 1 saved: \n',dish);
 		Dishes.create({
 			name: 'Paresh7', description: 'Maniyar7'
 		})
 		.then( (dish2) => {
-		console.log('Dish 2 saved: ',dish2);
-		return Dishes.find({}).exec();	//finds everthing, exec() ensures execution
+			console.log('Dish 2 saved: \n',dish2);
+			return Dishes.findByIdAndUpdate(dish._id, {
+				$set: { description: 'Maniyar7 Updated'}
+			},{
+				new: true// This second flag that we are supplying here, new: true, means that, once the update of the dish is complete, then this will return the updated dish back 
+			})
+			.exec();	//finds everthing, exec() ensures execution
 		})
-		.then( (dishes) => {
-			console.log('found dishes',dishes);
+		.then((dish) => {
+			console.log('Updated successfully: \n',dish);
+			dish.comments.push({
+				rating: 5,
+				comment: 'I can comment, but chill..Wont judge',
+				author: 'Jack of All Trade and of Titanic'
+			});
+			return dish.save();
+		})
+		.then( (dish) => {
+			console.log('Updated comments in this dish: \n',dish);
+			return Dishes.find({}).exec();
+		})
+		.then((dishes) => {
+			console.log("Full dishes list: \n", dishes);
 			return mongoose.connection.db.collection('dishes').drop();
 		})
 		.then( () => {
--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;//1 load
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
 const dishSchema = new Schema({ //2 define a model
 	name: {
 		type: String,
@@ -9,7 +27,8 @@ const dishSchema = new Schema({ //2 define a model
 	description: {
 		type: String,
 		required: true
-	}
+	},
+	comments: [commentSchema]
 },{
 	timestamps: true  //3 take care of this, it has created and updated
 });
